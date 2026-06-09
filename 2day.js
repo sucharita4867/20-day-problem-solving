dd38db253be4773d3a80ab5e508abe738c78f1bb
sucharita4867 <sucharitasardar7@gmail.com>
2026-06-09 16:06:55 +0530

    complete 2 problem

--- 2day.js
+++ 2day.js
@@ -5,4 +5,17 @@ function reverseString(str) {
   }
   return reversed;
 }
-console.log(reverseString("Hello, World!"));
+// console.log(reverseString("Hello, World"));
+
+// problem 2
+function countVowels(str) {
+  let count = 0;
+  const vowels = "aeiouAEIOU";
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countVowels("Hello, World"));
