ddc068c762739874e38df1f5be111834c76f3333
sucharita4867 <sucharitasardar7@gmail.com>
2026-06-11 18:00:36 +0530

    added problem 3 complete

--- 4day.js
+++ 4day.js
@@ -13,4 +13,20 @@ countProperties({ a: 1, b: 2, c: 3, d: 3 });
 function mergeObjects(obj1, obj2) {
   return { ...obj1, ...obj2 };
 }
-console.log(mergeObjects({ a: 1 }, { b: 2 }));
+// console.log(mergeObjects({ a: 1 }, { b: 2 }));
+
+// problem 3
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+fizzBuzz(15);
