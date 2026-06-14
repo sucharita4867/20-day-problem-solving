c58067b28d81c472fc3e1e87c8304cb7809a0106
sucharita4867 <sucharitasardar7@gmail.com>
2026-06-14 19:08:48 +0530

    added problem 27

--- 6day.js
+++ 6day.js
@@ -7,6 +7,12 @@ const user = {
 
 const { firstName: name, lastName, age } = user;
 
-console.log(name);
-console.log(lastName);
-console.log(age);
+// console.log(name);
+// console.log(lastName);
+// console.log(age);
+
+// problem 27
+function mergeArrays(...arrays) {
+  return arrays.flat();
+}
+console.log(mergeArrays([1, 2], [3, 4], [5, 6]));
