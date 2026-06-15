4042155236aa37837d82cd3c2fb9a726e8c88a77
sucharita4867 <sucharitasardar7@gmail.com>
2026-06-15 19:25:43 +0530

    added 33 problem

--- 7day.js
+++ 7day.js
@@ -11,14 +11,34 @@ function debounce(fn, delay) {
   };
 }
 
-// Example
-function search() {
-  console.log("Searching...");
+// problem 32
+function throttle(fn, limit) {
+  let lastCall = 0;
+
+  return function () {
+    const now = Date.now();
+
+    if (now - lastCall >= limit) {
+      lastCall = now;
+      fn();
+    }
+  };
 }
 
-const debouncedSearch = debounce(search, 300);
+// problem 33
+function deepClone(obj) {
+  if (obj === null || typeof obj !== "object") {
+    return obj;
+  }
 
-debouncedSearch();
-debouncedSearch();
-debouncedSearch();
-debouncedSearch();
+  const clone = Array.isArray(obj) ? [] : {};
+
+  for (let key in obj) {
+    clone[key] = deepClone(obj[key]);
+  }
+
+  return clone;
+}
+const original = { a: 1, b: { c: 2 } };
+const cloned = deepClone(original);
+cloned.b.c = 3;
