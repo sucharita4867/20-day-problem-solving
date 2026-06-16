9d25c2d7013593acd569e4396954f68727439b82
sucharita4867 <sucharitasardar7@gmail.com>
2026-06-16 23:37:47 +0530

    added next problem

--- 8day.js
+++ 8day.js
@@ -6,3 +6,16 @@ function safeJsonParse(str) {
     return null;
   }
 }
+
+// problem 37
+async function retry(fn, times) {
+  for (let i = 0; i < times; i++) {
+    try {
+      return await fn();
+    } catch (error) {
+      if (i === times - 1) {
+        throw error;
+      }
+    }
+  }
+}
