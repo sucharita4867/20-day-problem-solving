ba05e23d6658cc0fc0dfca7c2a900b2b5cb933b8
sucharita4867 <sucharitasardar7@gmail.com>
2026-06-18 22:44:00 +0530

    problem 42

--- 9day.js
+++ 9day.js
@@ -14,3 +14,10 @@ function twoSum(arr, target) {
 }
 
 console.log(twoSum([2, 7, 11, 15], 9));
+
+// problem 42
+function isAnagram(str1, str2) {
+  return str1.split("").sort().join("") === str2.split("").sort().join("");
+}
+
+console.log(isAnagram("listen", "silent"));
