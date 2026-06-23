708091050ece2f485c979d20ea04ecd4ed0f8094
sucharita4867 <sucharitasardar7@gmail.com>
2026-06-23 23:51:38 +0530

    added 49 question

--- 10day.js
+++ 10day.js
@@ -19,3 +19,28 @@ function rotateArray(arr, k) {
 
   return arr.slice(-k).concat(arr.slice(0, -k));
 }
+
+// problem 49
+function romanToInt(s) {
+  const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (values[s[i]] < values[s[i + 1]]) {
+      total -= values[s[i]];
+    } else {
+      total += values[s[i]];
+    }
+  }
+
+  return total;
+}
