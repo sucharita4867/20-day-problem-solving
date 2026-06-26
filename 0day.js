165b239eba964a678866e594b6a3b7ada04e88ed
sucharita4867 <sucharitasardar7@gmail.com>
2026-06-26 23:20:37 +0530

    added 5

--- 0day.js
+++ 0day.js
@@ -40,3 +40,19 @@ function isSorted(arr) {
 
   return true;
 }
+// problem 5
+function secondLargest(arr) {
+  let largest = -Infinity;
+  let second = -Infinity;
+
+  for (let num of arr) {
+    if (num > largest) {
+      second = largest;
+      largest = num;
+    } else if (num > second && num !== largest) {
+      second = num;
+    }
+  }
+
+  return second;
+}
