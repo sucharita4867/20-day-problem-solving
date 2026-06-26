60fb7dda90ab94bed722bb3d5d1eed838a6934ab
sucharita4867 <sucharitasardar7@gmail.com>
2026-06-26 23:19:19 +0530

    added probem 4

--- 0day.js
+++ 0day.js
@@ -29,3 +29,14 @@ function findDuplicates(arr) {
 
   return [...duplicates];
 }
+
+// problem4
+function isSorted(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+}
