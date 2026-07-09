a7bed3c869b23ed497e2131545a5e99b82bab43d
sucharita4867 <sucharitasardar7@gmail.com>
2026-07-09 22:15:41 +0530

    sum of odd numbers probelm 28

--- 0day.js
+++ 0day.js
@@ -296,3 +296,16 @@ function findLargest(arr) {
 
   return largest;
 }
+
+// Problem 28: Sum of Odd Numbers
+function sumOdd(arr) {
+  let sum = 0;
+
+  for (let num of arr) {
+    if (num % 2 !== 0) {
+      sum += num;
+    }
+  }
+
+  return sum;
+}
