8aabf4901d0e7dc97ef1a441b8ce117b558c6fa6
sucharita4867 <sucharitasardar7@gmail.com>
2026-08-05 22:42:08 +0530

    added reserse a string without using reverse

--- 0day.js
+++ 0day.js
@@ -383,3 +383,13 @@ function sumPositive(arr) {
 
   return sum;
 }
+// Problem 36: Reverse a String Without Using reverse()
+function reverseString(str) {
+  let result = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+
+  return result;
+}
