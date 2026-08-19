3ac607ba243a4b8335bb212e6033a8341fc07216
sucharita4867 <sucharitasardar7@gmail.com>
2026-08-19 23:18:56 +0530

    added new file problem first problem chake positive negative and zero number

---
+++ all-practice.js
@@ -0,0 +1,9 @@
+// Problem 1: Check Positive, Negative or Zero 
+const number = -8
+if(number > 0){
+console.log('Positive no :', number)
+} else if (number < 0){
+      console.log('Negative no:', number)
+}else{
+      console.log('Zero no:', number)
+}
