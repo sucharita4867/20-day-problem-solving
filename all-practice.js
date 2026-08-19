f8faa94d419e54b0a467265ac4fd66cf1eaecbd3
sucharita4867 <sucharitasardar7@gmail.com>
2026-08-19 23:52:20 +0530

    complete 2 no problem

--- all-practice.js
+++ all-practice.js
@@ -1,15 +1,17 @@
-// Problem 1: Check Positive, Negative or Zero 
-const number = -8
-if(number > 0){
-console.log('Positive no :', number)
-} else if (number < 0){
-      console.log('Negative no:', number)
-}else{
-      console.log('Zero no:', number)
+// Problem 1: Check Positive, Negative or Zero
+const number = -8;
+if (number > 0) {
+  console.log("Positive no :", number);
+} else if (number < 0) {
+  console.log("Negative no:", number);
+} else {
+  console.log("Zero no:", number);
 }
 
 // Problem 2: Check Voting Eligibility [Easy]
-const canVote = 34
-if(canVote >= 18){
-      console.log('Eligible')
+const canVote = 14;
+if (canVote >= 18) {
+  console.log("Eligible");
+} else {
+  console.log("Not Eligible");
 }
